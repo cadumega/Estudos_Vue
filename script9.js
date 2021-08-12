2b79cda2dffdafd63b8eef03f50c2e2f47beeb40
Cadu <cadumega@outlook.com>
2021-08-12 15:01:14 -0300

    Sintaxe de template: Interpolações

---
+++ script9.js
@@ -0,0 +1,11 @@
+let app = new Vue ({                      // Simples associação
+  el:'#app',
+  data:{
+    titulo:'Título da página',
+    subtitulo:'Este é o <strong>sub titulo</strong> da página',
+    copyright:'Todos os direitos reservados.',
+    nome:'Carlos',
+
+    carregado:true
+  }
+});
